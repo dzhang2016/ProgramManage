64dcad28c56b106d5916eb456192fe4da628b9f0
dzhang2016 <dzhang8989@hotmail.com>
2016-06-26 21:40:57 -0400

    Revert "Add more change in test functino 3."
    This reverts commit 8919c432a1ec13370854be5a6411f34c36e6bc3d.

--- src/main/java/com/domi/service/CourseServiceImpl.java
+++ src/main/java/com/domi/service/CourseServiceImpl.java
@@ -35,8 +35,7 @@ public class CourseServiceImpl implements CourseService {
 	}
 	
 	public void testFunction3() {
-		int tt = 18;
-		int k = 12;
+		int k = 11;
 		for(int i = 10; i<100; i++) {
 			System.out.println("Testing now");
 		}
